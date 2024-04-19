1a1b8e87e2ccf76dac714dadd0084757ad8a3c80
nickhumberstone <nickhumberstone@gmail.com>
2024-04-19 21:38:49 +0100

    DailyRouter functionality works

--- src/stacks/QuestionStack.js
+++ src/stacks/QuestionStack.js
@@ -1,7 +1,7 @@
 import QuestionScreen from '../screens/QuestionScreen';
 
-export default function AppStack({latestResponse}) {
+export default function AppStack({latestResponse, setResponse}) {
   return (
-    <QuestionScreen latestResponse={latestResponse}/>    
+    <QuestionScreen latestResponse={latestResponse} setResponse={setResponse}/>    
   );
 }
