d5142468e25247d22aec1338fbb35b6fcc8c8a29
nickhumberstone <nickhumberstone@gmail.com>
2024-04-21 22:54:18 +0100

    improved design of questioncard and questionscreen

--- src/screens/QuestionScreen.js
+++ src/screens/QuestionScreen.js
@@ -41,28 +41,26 @@ useEffect(() => {
     };
 
     return (
-<ScrollView contentContainerStyle={{ minHeight: '100%' }} className="bg-white">
+<ScrollView keyboardVerticalOffset={500} contentContainerStyle={{ minHeight: '100%' }} className="bg-white">
         <View className="flex-1 flex items-center justify-center mx-6 mt-10">
-        <Text className="text-4xl text-center text-[#627bb1] font-bold">Daily Question</Text>
+        {/* <Text className="text-4xl text-center text-[#627bb1] font-bold">Daily Question</Text> */}
         <Image className="aspect-square h-80" source={require('../assets/images/womanWriting.jpg')} />
-              
-    <DailyQuestionCard/>
-        <KeyboardAvoidingView className="p-4 bg-[#627bb1] shadow-lg shadow-black flex rounded-xl w-4/5">
+        <DailyQuestionCard/>      
+    
+        <KeyboardAvoidingView keyboardVerticalOffset={500} className="p-4 bg-[#627bb1] shadow-lg shadow-black flex flex-row rounded-xl w-9/10 gap-2">
     <TextInput
-    className="bg-blue-200 my-2 py-6 rounded-lg text-xl text-center"
+    className="bg-blue-200 py-6 rounded-lg text-xl text-center w-4/5"
     placeholder='Answer here'
     value={answer}
     onChangeText={setAnswer}
     maxLength={140}
     multiline={true}
     />
-    
 <TouchableOpacity
     onPress={postAnswer}
-    className="p-2 bg-white rounded-lg">
-<Text className="text-center text-xl text-[#667bb1]">{responseSubmitted ? "Response sent" : "Send it!"}</Text>
+    className="p-2 bg-white rounded-lg flex-1">
+<Text className="text-center text-xl text-[#667bb1]">{responseSubmitted ? "🎇" : "📩"}</Text>
       </TouchableOpacity>
-
   </KeyboardAvoidingView>
 
         <Text className="pt-1 px-1 text-gray-500 text-sm text-center">Answer the question above to enter the pool. Your answer must be 140 characters of less.</Text>
