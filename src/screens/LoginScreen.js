de0d403d85cf4453d2b6a28386104a1ba5ea9edc
nickhumberstone <nickhumberstone@gmail.com>
2024-09-08 10:31:09 +0100

    feat: improved login screen design

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,51 +1,57 @@
-import {View, Text, Image, ScrollView, TouchableOpacity} from 'react-native'
-import { useAuth0 } from 'react-native-auth0';
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+import { useAuth0 } from "react-native-auth0";
 
 export default function LoginScreen() {
+  const { authorize } = useAuth0();
 
-const {authorize} = useAuth0();
-  
-const login = async () => {
-  try {
+  const login = async () => {
+    try {
       await authorize();
-  } catch (e) {
+    } catch (e) {
       console.log(e);
-  }}
+    }
+  };
 
-    return (
-        <ScrollView contentContainerStyle={{ minHeight: '100%' }} className="bg-white">
-        <View>
-          <View className="flex-1 flex items-center justify-center mx-6 mt-20">
-          <Text className="text-3xl text-center text-[#627bb1] font-bold">Welcome to Retrospect</Text>
-        <Image className="aspect-square h-80" source={require('../assets/images/holdingform.png')}/>
-        <View className="p-4 bg-[#627bb1] shadow-lg shadow-black flex rounded-xl">
-        <Text className="py-2 mb-2 text-3xl text-center text-white font-bold">What is Retrospect?</Text>
-        <Text className="text-lg mb-4 text-white">Every day you will be asked a question.</Text>
-        <Text className="text-lg mb-4 text-white">What will your answer be?</Text>
-       
-        
-        <Text className="text-lg text-white">You can then see responses from the community, and your previous responses from the last month.</Text>
-        </View>
+  return (
+    <ScrollView
+      contentContainerStyle={{ minHeight: "100%" }}
+      className="bg-white"
+    >
+      <View>
+        <View className="flex-1 flex items-center justify-center mx-6 mt-20">
+          <Text className="text-3xl text-center text-[#627bb1] font-bold">
+            Welcome to Retrospect
+          </Text>
+          <Image
+            className="aspect-square h-60"
+            source={require("../assets/images/holdingform.png")}
+          />
+          <View className="p-2 bg-[#627bb1] shadow-lg shadow-black flex rounded-xl">
+            <Text className="mb-2 text-3xl text-center text-white font-bold">
+              What is Retrospect?
+            </Text>
+            <Text className="text-lg mb-4 text-white">
+              Every day you will be asked a question.
+            </Text>
+            <Text className="text-lg mb-4 text-white">
+              What will your answer be?
+            </Text>
 
-  <View className="p-2 flex-row">
-
-  <TouchableOpacity
-      onPress={login}
-      className="px-10 py-2 m-2 bg-blue-200 rounded-lg">
-        <Text className="text-center text-lg">Sign in</Text>
-  </TouchableOpacity>
-
-  <TouchableOpacity
-      onPress={login}
-      className="px-10 py-2 m-2 bg-blue-200 rounded-lg">
-        <Text className="text-center text-lg">Sign up</Text>
-  </TouchableOpacity>
-
-    </View>
-        </View>
+            <Text className="text-lg text-white">
+              You can then see responses from the community, and your previous
+              responses to the same question from the last month.
+            </Text>
           </View>
-        </ScrollView>
-       
-      
-    );
-  }
+          <View className="p-2">
+            <TouchableOpacity
+              onPress={login}
+              className="px-10 py-2 m-2 bg-blue-200 rounded-lg"
+            >
+              <Text className="text-center text-lg">Sign in</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
