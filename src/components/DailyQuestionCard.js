1c606f794e25e33d11045a5edba84def7ad2f4c7
nickhumberstone <nickhumberstone@gmail.com>
2024-09-11 16:37:55 +0100

    fixed question displaying by updating package.json script

--- src/components/DailyQuestionCard.js
+++ src/components/DailyQuestionCard.js
@@ -1,28 +1,39 @@
-import { KeyboardAvoidingView, Text } from 'react-native'
-import { useState, useEffect } from 'react';
-import React from 'react'
+import { KeyboardAvoidingView, Text } from "react-native";
+import { useState, useEffect } from "react";
+import React from "react";
 
 export default function DailyQuestionCard() {
+  const [question, setQuestion] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [question, setQuestion] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
   useEffect(() => {
     fetchDailyQuestion();
-  }, [])
-  
-    const fetchDailyQuestion = async() => {
-      const response = await fetch(`${process.env.EXPO_PUBLIC_SERVER_URL}/dailyquestion`)
-      const output = await response.json()
-    setQuestion(output[0].dailyQuestion)
-    setLoading(false)
+  }, []);
+
+  const fetchDailyQuestion = async () => {
+    const response = await fetch(
+      `${process.env.EXPO_PUBLIC_SERVER_URL}/dailyquestion`
+    );
+    const [output] = await response.json();
+    setQuestion(output.dailyQuestion);
+    setLoading(false);
   };
 
   return (
     <KeyboardAvoidingView className="w-full py-2 mb-6 bg-[#627bb1] shadow-lg shadow-black flex rounded-xl">
-      <Text className="p-1 text-center text-white text-lg">Today's question is:</Text>
-      {loading && (<Text className="text-center text-white px-2 pb-2 text-lg">~ Loading... ~</Text>)}
-    {question && (<Text className="text-center text-white px-2 pb-2 text-3xl font-bold">{question}</Text>)}
+      <Text className="p-1 text-center text-white text-lg">
+        Today's question is:
+      </Text>
+      {loading && (
+        <Text className="text-center text-white px-2 pb-2 text-lg">
+          ~ Loading... ~
+        </Text>
+      )}
+      {question && (
+        <Text className="text-center text-white px-2 pb-2 text-3xl font-bold">
+          {question}
+        </Text>
+      )}
     </KeyboardAvoidingView>
-  )
+  );
 }
--- src/routes/DailyRouter.js
+++ src/routes/DailyRouter.js
@@ -5,13 +5,15 @@ import { useAuth0 } from "react-native-auth0";
 import { useState, useCallback } from "react";
 import { useFocusEffect } from "@react-navigation/native";
 import { usePushNotifications } from "../../usePushNotifications";
+import { Text } from "react-native";
 
 export default function DailyRouter(props) {
   console.log("DailyRouter - props: ", props);
   const [answeredToday, setAnsweredToday] = useState(false);
   const [loading, setLoading] = useState(true);
 
-  const { expoPushToken, notification } = usePushNotifications(props.user);
+  // const { expoPushToken, notification } = usePushNotifications(props.user);
+
   // const data = JSON.stringify(notification, undefined, 2);
 
   // Whenever app becomes in focus, fetch the latest response for that user. Compare the timestamp to the current day to evaluate if they have answered today. Show QuestionStack if false, show Answerstack if true.
@@ -32,9 +34,10 @@ export default function DailyRouter(props) {
     console.log("Fetching to see if user has answered today");
     const response = await fetch(
       `${process.env.EXPO_PUBLIC_SERVER_URL}/didtheyanswertoday?` +
+        // new URLSearchParams({ user_id: "auth|7C663e6d028c9808c33cbfb0d2" })
         new URLSearchParams({ user_id: props.sub })
     );
-    const answers = await response.json();
+    const answers = await response;
     console.log("DidTheyAnswerToday? ", answers);
     setAnsweredToday(answers);
     setLoading(false);
@@ -54,6 +57,7 @@ export default function DailyRouter(props) {
       ) : (
         <LoadingScreen />
       )}
+      <Text>{answeredToday ? "true" : "false"}</Text>
       {/* <Text>Token: {expoPushToken?.data ?? ""}</Text>
       <Text>{data}</Text> */}
     </>
