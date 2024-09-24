2035f53fe3d39416f29c4a2c3dad2cea1360f89c
nickhumberstone <nickhumberstone@gmail.com>
2024-09-24 21:58:20 +0100

    updated app icon

--- app.config.js
+++ app.config.js
@@ -1,50 +1,54 @@
-const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_DEV = process.env.APP_VARIANT === "development";
 
-  export default {
-    name: IS_DEV ? "*Retrospect App": "Retrospect App",
-    slug: "retrospectapp",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./src/assets/icons/icon.png",
-    userInterfaceStyle: "light",
-    splash: {
-      image: "./src/assets/icons/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
+export default {
+  name: IS_DEV ? "*Retrospect App" : "Retrospect App",
+  slug: "retrospectapp",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./src/assets/icons/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./src/assets/icons/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: IS_DEV
+      ? "com.nickhumberstone.retrospectappdev"
+      : "com.nickhumberstone.retrospectapp",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./src/assets/icons/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: IS_DEV ? "com.nickhumberstone.retrospectappdev" : "com.nickhumberstone.retrospectapp"
+    package: IS_DEV
+      ? "com.nickhumberstone.retrospectappdev"
+      : "com.nickhumberstone.retrospectapp",
+    googleServicesFile: IS_DEV
+      ? process.env.GOOGLE_SERVICES_DEV_JSON
+      : process.env.GOOGLE_SERVICES_JSON,
+  },
+  web: {
+    favicon: "./src/assets/icons/favicon.png",
+  },
+  extra: {
+    eas: {
+      projectId: "10ae2907-876f-4704-843c-f9fba4384d1d",
     },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./src/assets/icons/adaptive-icon.png",
-        backgroundColor: "#ffffff"
+  },
+  plugins: [
+    [
+      "react-native-auth0",
+      {
+        domain: "dev-questionanswer.uk.auth0.com",
       },
-      package: IS_DEV ? "com.nickhumberstone.retrospectappdev": "com.nickhumberstone.retrospectapp",
-      googleServicesFile: IS_DEV ? process.env.GOOGLE_SERVICES_DEV_JSON : process.env.GOOGLE_SERVICES_JSON,
-    },
-    web: {
-      favicon: "./src/assets/icons/favicon.png"
-    },
-    extra: {
-      eas: {
-        projectId: "10ae2907-876f-4704-843c-f9fba4384d1d"
-      }
-    },
-    plugins: [
-      [
-        "react-native-auth0",
-        {
-          domain: "dev-questionanswer.uk.auth0.com"
-        }
-      ]
     ],
-    runtimeVersion: "1.0.0",
-    updates: {
-      url: "https://u.expo.dev/75d08780-7494-4738-97d1-19856d9d0675"
-    }
-  }
+  ],
+  runtimeVersion: "1.0.0",
+  updates: {
+    url: "https://u.expo.dev/75d08780-7494-4738-97d1-19856d9d0675",
+  },
+};
